1978c23a5f9912b52ff9cabe97a171d6ec9fbbf0
ekkatzhns <s2510892036@fhooe.at>
2026-06-10 18:08:07 +0200

    update

--- controller.js
+++ controller.js
@@ -4,3 +4,56 @@ Stelle sicher, dass der Controller reagiert, wenn sich die Gesamtnoten im Model 
  */
 
 'use strict';
+export class GraderController {
+
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+    init() {
+
+        this.view.renderInputs();
+        this.addInputListeners();
+        this.model.addEventListener("gradeChanged", (event) => {
+            this.view.renderResult(event.detail);
+        });
+        this.view.renderResult(this.model.getResult());
+
+    }
+
+    addInputListeners() {
+
+        document.addEventListener("change", (event) => {
+
+            const input = event.target;
+            if (input.tagName !== "INPUT") {
+                return;
+            }
+            const value = Number(input.value);
+            if (value < 0) {
+                input.value = 0;
+            }
+            if (value > 100) {
+                input.value = 100;
+            }
+
+            const correctedValue = Number(input.value);
+
+            if (input.id.startsWith("exercise-")) {
+                const index = Number(input.id.split("-")[1]);
+                this.model.setExercisePoints(index, correctedValue);
+            }
+
+            if (input.id === "exam") {
+                this.model.setExamPoints(correctedValue);
+            }
+            if (input.id === "attendance") {
+                this.model.setAttendance(correctedValue);
+
+            }
+
+        });
+
+    }
+
+}
